4c877a057c2e225fb8a2e3b5ad30e292a855a4a2
smart <smart@digitalwitchng.online>
2024-12-14 06:49:49 +0100

    deploy to GCP

--- 2_vpc.tf
+++ 2_vpc.tf
@@ -27,8 +27,6 @@ resource "google_compute_network" "main" {
 
 # Create Subnet
 # ==============
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 resource "google_compute_subnetwork" "private" {
   name                     = "private"
   ip_cidr_range            = "10.0.0.0/18"
